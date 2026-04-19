0d003d57eab6a7c258a10c223bf50c0ad8aea1d5
Mohamed Tamer Mohamed Ibrahim Saleh <144170765+MohamedO6@users.noreply.github.com>
2026-04-19 15:30:06 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -11,7 +11,8 @@ EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_TO = os.getenv("EMAIL_TO")
 
 BLOCK_PHRASES = [
-    "test-only-phrase-that-does-not-exist"
+    "sorry, you have been blocked",
+    "blocked"
 ]
 
 def send_email(subject, body):
